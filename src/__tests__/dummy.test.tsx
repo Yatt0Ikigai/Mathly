0895e994c66c70ca755fcc81348c22c567d83a40
aviv-piotrberestka <=>
2025-02-23 22:39:21 +0100

    feat: add tests setup

---
+++ src/__tests__/dummy.test.tsx
@@ -0,0 +1,15 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("a", () => {
+  render(<App />);
+  expect(screen.getByText(/count/i)).toBeDefined()
+});
